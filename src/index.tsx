4eaa8e4212a29d1727e0160b7ed35b0ef984966c
Ruben de Groot <rpg.degroot@gmail.com>
2022-08-08 23:52:45 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TikTokAutomate from './views/TikTokAutomate';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"><App /></Route>
+      <Route path="/tiktok-automate"><TikTokAutomate/></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

e5c7e2ea5615b65a8742ceaef20e11ef6f2cf1e8
Ruben de Groot <rpg.degroot@gmail.com>
2022-11-15 23:41:01 +0100

    Professionalize

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,20 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Welkom jochie!</h1>
-        <Link to='/tiktok-automate'>TikTok</Link>
+        <h1
+          style={{
+            marginBottom: '0px'
+          }}
+        >DEVELOPER BASED IN AMSTERDAM</h1>
+
+        <h2 style={{
+            marginBottom: '0px'
+          }}>Website coming soon...</h2>
+        <h3><a href='mailto:info@rubendegroot.com'>info@rubendegroot.com</a></h3>
+        <div>
+        <a href='https://instagram.com/rubengroot_'>Instagram</a>&nbsp;&nbsp;
+        <a href='https://www.linkedin.com/in/ruben-d-950385199/'>LinkedIn</a>
+        </div>
       </header>
     </div>
   );
